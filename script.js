a96c102e8447f2744293b13476782a5a9fd53a8b
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-10-13 15:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let form = document.querySelector("#form-test");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let age = document.querySelector("#age").value;
+    let name = document.querySelector("#name").value;
+
+    const newPromise = () => {
+        return new Promise((resolve, reject) => {
+            if (age > 18) {
+                setTimeout(() => {
+                    alert(`Welcome, ${name}. You can vote.`);
+                    resolve();
+                }, 4000);
+            } else {
+                alert(`Oh sorry ${name}. You aren't old enough.`);
+                reject();
+            }
+        });
+    };
+
+    newPromise().catch(() => {});
+});
